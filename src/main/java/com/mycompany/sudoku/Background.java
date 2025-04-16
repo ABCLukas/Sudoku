d174265d841d143ccd4fa9910184a526f763ad5e
Lukas <153985968+ABCLukas@users.noreply.github.com>
2025-04-16 15:03:34 +0200

    Update Background.java
    add vertical

--- src/main/java/com/mycompany/sudoku/Background.java
+++ src/main/java/com/mycompany/sudoku/Background.java
@@ -166,27 +166,6 @@ public class Background {
         }
     }
 
-//    public void findVerticalUniqueNumbers(Gamefield gf, int row) {
-//        int[] numberListCounter = new int[9];
-//        //Count Unique numbers
-//        for (int i = 0; i < 9; i++) {
-//            for (int j = 0; j < background[row][i].size(); j++) {
-//                numberListCounter[background[row][i].get(j) - 1]++;
-//            }
-//        }
-//
-//        for (int i = 0; i < 9; i++) {
-//            if (numberListCounter[i] == 1) {
-//                for (int j = 0; j < 9; j++) {
-//                    if (background[row][j].contains(numberListCounter[i])) {
-//                        gf.insertNumber(numberListCounter[i], row, j);
-//                        resetCell(row, j);
-//                    }
-//                }
-//            }
-//        }
-//    }
-
     public void findHorizontalUniqueNumbers(Gamefield gf, int column) {
 
         int[] numberListCounter = new int[9];
@@ -214,23 +193,23 @@ public class Background {
 
     
     //not final
-    public void findVerticalUniqueNumbers(Gamefield gf, int column) {
+    public void findVerticalUniqueNumbers(Gamefield gf, int row) {
 
         int[] numberListCounter = new int[9];
 
         for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < background[i][column].size(); j++) {
-                int zahl = background[i][column].get(j);
+            for (int j = 0; j < background[row][i].size(); j++) {
+                int zahl = background[row][i].get(j);
                 numberListCounter[zahl - 1]++;
             }
         }
 
         for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < background[i][column].size(); j++) {
-                int zahl = background[i][column].get(j);
+            for (int j = 0; j < background[row][i].size(); j++) {
+                int zahl = background[row][i].get(j);
                 if (numberListCounter[zahl - 1] == 1) {
 
-                    gf.insertNumber(zahl, i, column);
+                    gf.insertNumber(zahl, row, i);
 
                     gamefieldScanner(gf);
                     break;
